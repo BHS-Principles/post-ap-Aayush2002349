2260207d750996af2a2ffe49b059f63c000da8b8
Aayush2002349 <aayush15genius@gmail.com>
2025-05-21 16:30:02 +0000

    more style stuff

--- js2.js
+++ js2.js
@@ -1,13 +1,39 @@
 var action = function(event){
     alert(event)
+    console.log(event)
 }
 var action2 = function(key){
-    alert(key.key)
+    var x = Number(square.style.top.substring(0,square.style.top - 2))
+    var y = Number(square.style.left.substring(0,square.style.left - 2))
+    alert(x)
+    alert(y)
+    if(key.key == "w"){
+        y -= 1
+    }
+    if(key.key == "s"){
+        y += 1
+    }
+    if(key.key == "a"){
+        x -= 1
+    }
+    if(key.key == "d"){
+        x += 1
+    }
+
+    square.style.left = String(x) + "px"
+    square.style.top = String(y) + "px"
 }
 
 var list = ["a","b","c","d","e","f","g","h"];
 
 var btn = document.getElementById("button");
+var square = document.createElement("area")
+
+square.style.backgroundColor = "rgb(198, 243, 247)"
+square.style.position = "absolute"
+document.body.append(square)
+square.textContent = "abcdefghi"
+
 for(var i = 0; i < list.length; i++){
     var newBtn = btn.cloneNode(true);
     document.body.append(newBtn);
@@ -15,4 +41,5 @@ for(var i = 0; i < list.length; i++){
 
     newBtn.addEventListener("click",action)
 }
+
 document.addEventListener("keydown",action2)
