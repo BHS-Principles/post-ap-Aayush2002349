03f76eeea638b915e862b8e7a69b486c5dd3200a
Aayush2002349 <aayush15genius@gmail.com>
2025-05-21 15:56:08 +0000

    style stuff

--- js2.js
+++ js2.js
@@ -1,14 +1,18 @@
+var action = function(event){
+    alert(event)
+}
+var action2 = function(key){
+    alert(key.key)
+}
+
 var list = ["a","b","c","d","e","f","g","h"];
 
 var btn = document.getElementById("button");
 for(var i = 0; i < list.length; i++){
     var newBtn = btn.cloneNode(true);
-    newBtn.innerHTML = list[i];
     document.body.append(newBtn);
+    newBtn.innerHTML = list[i];
 
     newBtn.addEventListener("click",action)
-
 }
-var action = function(event){
-    alert(event)
-}
+document.addEventListener("keydown",action2)
