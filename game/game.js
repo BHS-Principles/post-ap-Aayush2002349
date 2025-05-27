7dae1baaf2e5ab9113576d0b8b5fd38c6bffddea
Aayush2002349 <aayush15genius@gmail.com>
2025-05-27 17:01:47 +0000

    basic game structure

---
+++ game/game.js
@@ -0,0 +1,13 @@
+class Cards{
+    constructor(type,numVal,strVal){
+        this.type = type
+        this.num = numVal
+        this.str = strVal
+    }
+}
+
+var allCardsInDeck = []
+var discardedCards = []
+var currentCard = null
+var toDrawCards = []
+var cardsInShop = []
