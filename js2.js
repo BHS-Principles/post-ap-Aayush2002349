3f9d4960cddc48779c67abc1a07891eab8cbb4a4
Aayush2002349 <aayush15genius@gmail.com>
2025-05-27 16:14:46 +0000

    just some organizing

--- js2.js
+++ js2.js
@@ -2,38 +2,10 @@ var action = function(event){
     alert(event)
     console.log(event)
 }
-var action2 = function(key){
-    console.log(square.style)
-    var x = Number(square.style.top.substring(0,square.style.top - 2))
-    var y = Number(square.style.left.substring(0,square.style.left - 2))
-    if(key.key == "w"){
-        y -= 1
-    }
-    if(key.key == "s"){
-        y += 1
-    }
-    if(key.key == "a"){
-        x -= 1
-    }
-    if(key.key == "d"){
-        x += 1
-    }
-
-    square.style.left = String(x) + "px"
-    square.style.top = String(y) + "px"
-}
 
 var list = ["a","b","c","d","e","f","g","h"];
 
 var btn = document.getElementById("button");
-var square = document.createElement("area")
-
-square.style.backgroundColor = "rgb(198, 243, 247)"
-square.style.position = "absolute"
-square.style.top = "100px"
-square.style.left = "100px"
-document.body.append(square)
-square.textContent = "abcdefghi"
 
 for(var i = 0; i < list.length; i++){
     var newBtn = btn.cloneNode(true);
@@ -44,3 +16,42 @@ for(var i = 0; i < list.length; i++){
 }
 
 document.addEventListener("keydown",action2)
+
+
+
+var action2 = function(key){
+    console.log(square.style)
+    if(key.key == "w"){
+        y -= 10
+    }
+    if(key.key == "s"){
+        y += 10
+    }
+    if(key.key == "a"){
+        x -= 10
+    }
+    if(key.key == "d"){
+        x += 10
+    }
+
+    square.style.left = String(x) + "px"
+    square.style.top = String(y) + "px"
+}
+
+var square = document.createElement("area")
+
+square.style.backgroundColor = "rgb(198, 243, 247)"
+square.style.position = "absolute"
+square.style.top = "100px"
+square.style.left = "100px"
+var x = 100
+var y = 100
+document.body.append(square)
+square.textContent = "abcdefghi"
+
+var inputmap = {
+    w:false,
+    s:false,
+    a:false,
+    d:false
+}
