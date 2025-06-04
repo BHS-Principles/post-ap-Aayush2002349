61eff5bbb852684322dea34c9ae9f6a037752564
Aayush2002349 <aayush15genius@gmail.com>
2025-06-04 15:37:16 +0000

    More organizing, also now starting to make art and draw the cards

--- game/game.js
+++ game/game.js
@@ -1,4 +1,4 @@
-//If adding new card, add it to allCards, add it to card sheet, change scaling in css and add its card action to cardAction
+//If adding new card, add it to allCards, add it to card sheet, change scaling in css and add its card action to cardAction and draw, both its action and its position
 
 
 
@@ -21,6 +21,45 @@ class Cards{
     //Deep copies a card
     deepCopy(){
         return {type:this.type, num:this.num, str:this.str, cost:this.cost};
+    }
+
+    draw(xPos,yPos){
+
+        //Gets html elements
+        var temp = document.getElementById("temp");
+        var target = document.getElementById("target")
+        var cardCopy = temp.querySelector(".Card");
+        
+        //Gets the card from the card sheet
+        var xCard = 0;
+        var yCard = 0;
+        if(this.type == "num"){
+            yCard = 0
+        }
+        if(this.type == "multPrev"){
+            yCard = 1
+        }
+        if(this.type == "chance"){
+            yCard = 2
+        }
+        if(this.type == "multNext"){
+            yCard = 3
+        }
+        
+        xCard = this.id
+
+        //Creating the card
+        var card = cardCopy.cloneNode(true)
+
+        //Displays which card it is
+        card.style.backgroundPositionX = "" + 10*xCard + "rem"
+        card.style.backgroundPositionY = "" + 15*yCard + "rem"
+
+        //Puts where the card is on the screen
+        card.style.left = "" + xPos + "rem"
+        card.style.top = "" + yPos + "rem"
+
+        target.append(currentCardDisplay)
     }
 }
 
@@ -76,7 +115,7 @@ var deepCopyCardList = function(cards){
 }
 
 //Gets a random card
-var getRandomCard = function(allCards){
+var getRandomCard = function(allCards,maxPoints){
     
     var currentCard = allCards[Math.floor(Math.random()*allCards.length)];
 
@@ -87,21 +126,21 @@ var getRandomCard = function(allCards){
 
     var card = new Cards(type,num,str,numSelect);
 
-    var cost = (numSelect+1) * currentCard.cost;
+    var cost = Math.ceil(((numSelect+1) * currentCard.cost)/2) * Math.ceil((maxPoints + 1)/8);
     card.setCost(cost);
     return card;
 }
 
 //Gets n random cards (which is used to refresh the shop)
-var refresh = function(allCards,num){
+var refresh = function(allCards,maxPoints,num){
     var cards = [];
     for(var i = 0; i < num; i++){
-        cards.push(getRandomCard(allCards));
+        cards.push(getRandomCard(allCards,maxPoints));
     }
     return cards;
 };
 
-//Draw a random card
+//Draw a random card (everything is local)
 var drawCard = function(currentCard,drawPile,discardPile,state){
     //Reshuffle
     if(drawPile.length == 0){
@@ -115,9 +154,12 @@ var drawCard = function(currentCard,drawPile,discardPile,state){
     currentCard = drawPile.pop(drawnIndex)
     
     //Does all the card actions
+    var info = doCardAction(currentCard,state)
+
+    return {points:info.points,state:info.state,currentCard:currentCard,drawPile:drawPile,discardPile:discardPile}
 }
 
-var cardAction(currentCard,state){
+var doCardAction = function(currentCard,state){
     var points = 0
 
     //All different cards and their behaviors
@@ -133,6 +175,9 @@ var cardAction(currentCard,state){
         } else {
             points = 0
         }
+    }
+    if(currentCard.type == "multNext"){
+        state.nextMults.push({num: Math.floor(currentCard.num / 10) , type:1 + currentCard.num % 10})
     }
 
 
@@ -150,28 +195,38 @@ var cardAction(currentCard,state){
     }
 
     state.prev = points
+
+    return {points:points,state:state}
 }
+
+//Call this function every time a card is drawn, it will do all the math and display stuff
+var onCardDraw = function(){
+
+    //All the math is complete
+    var info = drawCard(currentCard,drawPile,discardPile,state)
+
+    totalPoints += info.points
+    state = info.state
+    drawPile = info.drawPile
+    discardPile = info.discardPile
+    currentCard = info.currentCard
+}
+
 
 var allCardsInDeck = createStartingCards(allCards);
 var discardPile = [];
 var currentCard = null;
 var drawPile = deepCopyCardList(allCardsInDeck);
-var cardsInShop = refresh(allCards,5);
+var cardsInShop = refresh(allCards,maxPoints,5);
 
 var state = {
     prevVal:0,
     nextMults:[]
 }
 
+var totalPoints = 0
+var maxPoints = 0
+
 var temp = document.getElementById("temp");
 var target = document.getElementById("target")
 var cardDisplay = temp.querySelector(".Card");
-
-for(var i = 0; i < 52; i++){
-    var currentCardDisplay = cardDisplay.cloneNode(true)
-    currentCardDisplay.style.backgroundPositionX = "" + 10*(i%13) + "rem"
-    currentCardDisplay.style.backgroundPositionY = "" + 15*(Math.floor(i/13))+ "rem"
-    currentCardDisplay.innerHTML = i
-
-    target.append(currentCardDisplay)
-}
