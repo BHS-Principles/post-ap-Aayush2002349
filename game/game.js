ad74a0b6fc97f86689f819014c05aa944b9aa8fe
Aayush2002349 <aayush15genius@gmail.com>
2025-06-04 16:40:02 +0000

    Converting all functions to be inside classes

--- game/game.js
+++ game/game.js
@@ -5,12 +5,40 @@
 class Cards{
 
     //Creates a card
-    constructor(type,numVal,strVal,index){
-        this.type = type;
-        this.num = numVal;
-        this.str = strVal;
-        this.index = index;
-        this.cost = null;
+    constructor(){
+    }
+
+    //Sets the info of the card
+    setInfo(isBack,type,numVal,strVal,index){
+        if(isBack){
+            this.index = index;
+        } else {
+            this.type = type;
+            this.num = numVal;
+            this.str = strVal;
+            this.index = index;
+            this.cost = null;
+        }
+    }
+
+    makeRandomCard(allCards,maxPoints){
+
+        //Gets a random kind of card
+        var info = allCards[Math.floor(Math.random()*allCards.length)];
+
+        //Extracts all the info from the card
+        var type = info.type;
+        var index = Math.floor(Math.random()*info.num.length);
+
+        var num = info.num[index];
+        var str = info.str[Math.floor(Math.random()*info.str.length)];
+
+        var cost = Math.ceil(((index+1) * info.cost)/2) * Math.ceil((maxPoints + 1)/8);
+
+        //Sets the card's properties
+        this.setInfo(false,type,num,str,index);
+        this.setCost(cost);
+
     }
 
     //Sets the card's cost
@@ -20,9 +48,10 @@ class Cards{
 
     //Deep copies a card
     deepCopy(){
-        return {type:this.type, num:this.num, str:this.str, cost:this.cost};
+        return {type:this.type, num:this.num, str:this.str, cost:this.cost, index:this.index};
     }
 
+    //Draws the card onto the screen
     draw(xPos,yPos){
 
         //Gets html elements
@@ -34,32 +63,36 @@ class Cards{
         var xCard = 0;
         var yCard = 0;
         if(this.type == "num"){
-            yCard = 0
-        }
-        if(this.type == "multPrev"){
             yCard = 1
         }
-        if(this.type == "chance"){
+        if(this.type == "multPrev"){
             yCard = 2
         }
-        if(this.type == "multNext"){
+        if(this.type == "chance"){
             yCard = 3
         }
+        if(this.type == "multNext"){
+            yCard = 4
+        }
         
-        xCard = this.id
+        xCard = this.index
 
         //Creating the card
         var card = cardCopy.cloneNode(true)
 
         //Displays which card it is
-        card.style.backgroundPositionX = "" + 10*xCard + "rem"
-        card.style.backgroundPositionY = "" + 15*yCard + "rem"
+        card.style.backgroundPositionX = "" + -10*xCard + "rem"
+        card.style.backgroundPositionY = "" + -15*yCard + "rem"
 
         //Puts where the card is on the screen
+        card.style.position = "absolute"
         card.style.left = "" + xPos + "rem"
         card.style.top = "" + yPos + "rem"
 
-        target.append(currentCardDisplay)
+        console.log("" + xPos + "rem" + " " + yPos + "rem")
+
+        target.append(card)
+
     }
 }
 
@@ -98,7 +131,9 @@ var createStartingCards = function(allCards){
     var current = allCards[0];
 
     for(var i = 0; i < current.num.length; i++){
-        cards.push(new Cards(current.type,current.num[i],current.str[0],i));
+        var cardTemp = new Cards()
+        cardTemp.setInfo(false,current.type,current.num[i],current.str[0],i)
+        cards.push(cardTemp);
     }
 
     
@@ -114,28 +149,15 @@ var deepCopyCardList = function(cards){
     }
 }
 
-//Gets a random card
-var getRandomCard = function(allCards,maxPoints){
-    
-    var currentCard = allCards[Math.floor(Math.random()*allCards.length)];
-
-    var type = currentCard.type;
-    var numSelect = Math.floor(Math.random()*currentCard.num.length);
-    var num = currentCard.num[numSelect];
-    var str = currentCard.str[Math.floor(Math.random()*currentCard.str.length)];
-
-    var card = new Cards(type,num,str,numSelect);
-
-    var cost = Math.ceil(((numSelect+1) * currentCard.cost)/2) * Math.ceil((maxPoints + 1)/8);
-    card.setCost(cost);
-    return card;
-}
-
 //Gets n random cards (which is used to refresh the shop)
 var refresh = function(allCards,maxPoints,num){
     var cards = [];
     for(var i = 0; i < num; i++){
-        cards.push(getRandomCard(allCards,maxPoints));
+
+        var cardTemp = new Cards()
+        cardTemp.makeRandomCard(allCards,maxPoints)
+
+        cards.push(cardTemp);
     }
     return cards;
 };
@@ -227,6 +249,10 @@ var state = {
 var totalPoints = 0
 var maxPoints = 0
 
-var temp = document.getElementById("temp");
-var target = document.getElementById("target")
-var cardDisplay = temp.querySelector(".Card");
+
+//Testing drawing cards
+var testing = refresh(allCards,maxPoints,100);
+
+for(var i = 0; i < testing.length; i++){
+    testing[i].draw(10*(i%10),15*Math.floor(i/10))
+}
